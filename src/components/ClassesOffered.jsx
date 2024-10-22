c1707289d490259927033c1ae0d9cf8afb57c78c
Manish Pokhriyal <Manish.pokhriyal.666@gmail.com>
2024-10-22 11:13:30 +0530

    Update ClassesOffered.jsx

--- src/components/ClassesOffered.jsx
+++ src/components/ClassesOffered.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from './Navbar'; 
 import { useNavigate } from 'react-router-dom';
 import Footer from './Footer'; 
