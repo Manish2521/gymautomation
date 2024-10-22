4e65b7e8c47c5d589e8369c998310150a19e958c
Manish Pokhriyal <Manish.pokhriyal.666@gmail.com>
2024-10-22 10:58:24 +0530

    Update Table.jsx

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Table = () => {
   const cardsData = [
     {
-      title: 'Total Members',
+      title: 'Total Membership',
       value: 20,
       description: 'Total number of active gym members.',
       colorClass: 'bg-blue-500', 
@@ -13,49 +13,49 @@ const Table = () => {
     {
       title: 'Available Plans',
       value: 5,
-      description: 'Total number of plans available for members.',
+      description: 'Total number of plans available.',
       colorClass: 'bg-green-500',
       link: '/availableplans', 
     },
     {
       title: 'Gym Revenue',
       value: '$5000',
-      description: 'Total revenue generated this month.',
+      description: 'Total revenue.',
       colorClass: 'bg-purple-500',
       link: '/GymRevenue', 
     },
     {
       title: 'Gymers',
       value: 15,
-      description: 'Number of gym users accessing the facility.',
+      description: 'Number of gym users.',
       colorClass: 'bg-red-500',
       link: '/Gymers', 
     },
     {
       title: 'Employees',
       value: 4,
-      description: 'Total employees working in the gym.',
+      description: 'Total employees.',
       colorClass: 'bg-yellow-500',
       link: '/Employees', 
     },
     {
       title: 'Trainers',
       value: 2,
-      description: 'Total number of certified trainers available.',
+      description: 'Total number of certified trainers.',
       colorClass: 'bg-red-500',
       link: '/Trainers', 
     },
     {
       title: 'Gym Equipment',
       value: 23,
-      description: 'Total equipment available for gym members.',
+      description: 'Total equipment.',
       colorClass: 'bg-blue-500',
       link: '/GymEquipment',
     },
     {
       title: 'Classes Offered',
       value: 10,
-      description: 'Total number of classes available for members.',
+      description: 'Total number of classes.',
       colorClass: 'bg-indigo-500',
       link: '/ClassesOffered', 
     },
@@ -69,10 +69,10 @@ const Table = () => {
             <Link
               key={index}
               to={card.link}
-              className={`max-w-sm p-6 border border-gray-200 rounded-lg shadow ${card.colorClass} text-white text-center hover:shadow-lg transition-shadow duration-200`}
+              className={`max-w-sm p-8 border border-gray-200 rounded-lg shadow ${card.colorClass} text-white text-center hover:shadow-lg transition-shadow duration-200`}
             >
               <h5 className="mb-2 text-2xl font-bold">{card.title}</h5>
-              <p className="mb-3 text-3xl">{card.value}</p>
+              {/* <p className="mb-3 text-3xl">{card.value}</p> */}
               <p className="text-gray-200">{card.description}</p>
             </Link>
           ))}
