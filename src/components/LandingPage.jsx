bb0282f64098cb0b326813c591c7cbcff07a1531
Manish Pokhriyal <Manish.pokhriyal.666@gmail.com>
2024-10-22 10:27:16 +0530

    Check if user exist in db or not

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -7,6 +7,49 @@ import Table from './Table';
 const LandingPage = () => {
   const navigate = useNavigate();
 
+
+// Check if user exist in db or not  
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    const username = localStorage.getItem('username');
+    
+    const checkUserInDB = async () => {
+      try {
+        const response = await fetch(`https://gymautomation.onrender.com/checkUser?username=${username}`, {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        });
+        if (!response.ok) {
+          throw new Error('User not found or session expired');
+        }
+        const data = await response.json();
+        if (!data.exists) {
+          localStorage.removeItem('token');
+          localStorage.removeItem('username');
+          navigate('/error');
+        }
+      } catch (error) {
+        console.error('Error checking user:', error);
+        localStorage.removeItem('token');
+        localStorage.removeItem('username');
+        navigate('/error');
+      }
+    };
+  
+    if (token && username) {
+      checkUserInDB();
+    } else {
+      navigate('/error');
+    }
+  }, [navigate]);
+
+
+
+
+
+
+  
   useEffect(() => {
     const token = localStorage.getItem('token');
     if (!token) {
