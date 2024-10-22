e3821078f93bf42667d81d4c8b67fcb351ea1d2c
Manish Pokhriyal <Manish.pokhriyal.666@gmail.com>
2024-10-22 10:27:57 +0530

    Update Gymers.jsx

--- src/components/Gymers.jsx
+++ src/components/Gymers.jsx
@@ -17,6 +17,50 @@ const Gymers = () => {
         { name: 'Daniel Moore', membership: 'Monthly', joined: '2023-09-10', status: 'Inactive' },
         { name: 'Olivia Harris', membership: 'Quarterly', joined: '2023-05-25', status: 'Active' },
       ];
+
+
+// Check if user exist in db or not  
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    const username = localStorage.getItem('username');
+    
+    const checkUserInDB = async () => {
+      try {
+        const response = await fetch(`https://gymautomation.onrender.com/checkUser?username=${username}`, {
+          headers: {
+            'Authorization': `Bearer ${token}`
+          }
+        });
+        if (!response.ok) {
+          throw new Error('User not found or session expired');
+        }
+        const data = await response.json();
+        if (!data.exists) {
+          localStorage.removeItem('token');
+          localStorage.removeItem('username');
+          navigate('/error');
+        }
+      } catch (error) {
+        console.error('Error checking user:', error);
+        localStorage.removeItem('token');
+        localStorage.removeItem('username');
+        navigate('/error');
+      }
+    };
+  
+    if (token && username) {
+      checkUserInDB();
+    } else {
+      navigate('/error');
+    }
+  }, [navigate]);
+
+
+
+
+
+
+    
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
