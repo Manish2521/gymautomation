d2333eb61d4423f7792476e68dcf1c427509657a
Manish Pokhriyal <Manish.pokhriyal.666@gmail.com>
2024-11-08 23:11:01 +0530

    Update Error.jsx

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -22,12 +22,12 @@ const Error = () => {
   }, [countdown, navigate]);
 
   return (
-  <section className="flex items-center justify-center min-h-screen bg-white dark:bg-gray-900">
+  <section className="flex items-center justify-center min-h-screen bg-red-600 dark:bg-gray-900">
     <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 text-center">
       <div className="mx-auto max-w-screen-sm">
-        <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">403</h1>
-        <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-5xl dark:text-white">Access Denied</p>
-        <p className="mb-4 text-lg font-light text-black-500 dark:text-black-400">
+        <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 text-white">403</h1>
+        <p className="mb-4 text-3xl tracking-tight font-bold text-white md:text-5xl dark:text-white">Access Denied</p>
+        <p className="mb-4 text-lg font-light text-white dark:text-black-400">
           You'll be redirected to the Login page in {countdown} seconds.
         </p>
       </div>
